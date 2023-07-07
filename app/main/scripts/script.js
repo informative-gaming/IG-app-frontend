985ac8ed32f94ad0f220beec35fb179c42a42896
Luciousdev <lucy.puyenbroek@gmail.com>
2023-07-08 00:54:12 +0200

    Changes in the way the nav bar works.
    - Nav bar will now hide the content besides the ones requested to be seen.
    This removes the need for more html pages.

--- app/main/scripts/script.js
+++ app/main/scripts/script.js
@@ -12,3 +12,20 @@ console.log(date + "Info: All Content has been loaded succesfully");
 function elementLoaded(element) {
     console.log(date + `Info: ${element} has been loaded succesfully`);
 }
+
+
+// TRANSITIONS
+function showElement(elementId) {
+    const elements = ['main-page', 'game-list', 'your-games', 'ai', 'feedback', 'account'];
+
+    elements.forEach((element) => {
+        const el = document.getElementById(element);
+        if (element === elementId) {
+            el.classList.remove('hidden');
+            el.classList.add('visible');
+        } else {
+            el.classList.remove('visible');
+            el.classList.add('hidden');
+        }
+    });
+}
